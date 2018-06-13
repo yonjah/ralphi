9f56da62ef7234b815740f6739a1186e61da7ad8
Yoni Jah <yonjah@gmail.com>
2018-06-13 12:08:09 +0800

    style: added eslint markdown plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
         "es6": true,
         "node": true
     },
+    "plugins": [
+        "markdown"
+    ],
     "parserOptions": { "ecmaVersion": 2017 },
     "extends": "eslint:recommended",
     "rules": {
